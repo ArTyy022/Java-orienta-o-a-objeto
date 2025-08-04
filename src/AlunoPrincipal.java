6900b42afc7fb91bd81d9d0c69ed2e5df075be61
ArTyy022 <toledoarthur11@gmail.com>
2025-08-04 20:04:44 -0300

    Add files via upload

---
+++ src/AlunoPrincipal.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class AlunoPrincipal {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        aluno.nome = "Arthur";
+        aluno.idade = 18;
+
+        aluno.Exibir();
+    }
+}
